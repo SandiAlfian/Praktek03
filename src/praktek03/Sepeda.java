38d6216dc378e1d0b3336213dab3a3308f16c42e
Sandyal__ <sandialfiann00@gmail.com>
2023-05-28 03:27:01 +0800

    Buat Getter & Setter

--- src/praktek03/Sepeda.java
+++ src/praktek03/Sepeda.java
@@ -9,5 +9,30 @@ public class Sepeda {
                            "Warna \t : " + Warna + "\n" +
                            "Harga \t : " + Harga);
     }
+
+    public String getMerk() {
+        return Merk;
+    }
+
+    public void setMerk(String Merk) {
+        this.Merk = Merk;
+    }
+
+    public String getWarna() {
+        return Warna;
+    }
+
+    public void setWarna(String Warna) {
+        this.Warna = Warna;
+    }
+
+    public double getHarga() {
+        return Harga;
+    }
+
+    public void setHarga(double Harga) {
+        this.Harga = Harga;
+    }
+    
     
 }

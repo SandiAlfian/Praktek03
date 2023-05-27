9a37b2a3e43be8731cd7aa91f71bc0ae5d740fec
Sandyal__ <sandialfiann00@gmail.com>
2023-05-28 03:36:08 +0800

    Getter & Setter

--- src/praktek03/SepedaMotor.java
+++ src/praktek03/SepedaMotor.java
@@ -5,4 +5,14 @@ public class SepedaMotor extends Sepeda{
     void throttle(){
         System.out.println("NGEEEEEENG.......");
     }
+
+    public String getJenisMotor() {
+        return jenisMotor;
+    }
+
+    public void setJenisMotor(String jenisMotor) {
+        this.jenisMotor = jenisMotor;
+    }
+    
+    
 }

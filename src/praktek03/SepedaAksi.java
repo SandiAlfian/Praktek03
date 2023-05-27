9c0b5157feb21a122826c60d1d564b59e603fbc3
Sandyal__ <sandialfiann00@gmail.com>
2023-05-28 03:29:05 +0800

    Menggunakan Method Set

--- src/praktek03/SepedaAksi.java
+++ src/praktek03/SepedaAksi.java
@@ -3,9 +3,9 @@ public class SepedaAksi {
     public static void main(String[] args) {
         Sepeda s = new Sepeda();
         
-        s.Merk = "Kupu - Kupu";
-        s.Warna = "Merah";
-        s.Harga =  1000;
+        s.setMerk("ButterFly");
+        s.setWarna("Merah");
+        s.setHarga(1000);
         
         s.cetakInfo();
     }
